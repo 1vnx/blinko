cf78d0ccc8fe914d63da74196cc142eb030abae7
Swargaraj Bhowmik <swargarajbhowmik@gmail.com>
2024-07-24 11:49:45 +0530

    chore(landing): add particles

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,5 +1,21 @@
+import Link from "next/link";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { Sparkles } from "lucide-react";
+
 export default function Header() {
-    return <div style={{ height: "100vh" }} className="flex flex-col justify-center items-center">
-        <h1 className="text-5xl font-bold text-center justify-center">welcome to blinko!</h1>
-    </div>;
+  return (
+    <div className="relative flex justify-between items-center p-4 px-6 border-b z-50">
+      <div>
+        <Link href="/">
+          <Image src="/blinko.svg" alt="Blibko" width="90" height="50" />
+        </Link>
+      </div>
+      <div>
+        <Button variant="outline">
+          Create Profile <Sparkles className="h-4 w-4 ml-2 text-purple-300" />
+        </Button>
+      </div>
+    </div>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,23 @@
+"use client";
+
 import Header from "./_components/header";
+
+import Particles from "@/components/magicui/particles";
 
 export default function Landing() {
   return (
-    <div>
-      <Header />
+    <div className="p-6 bg-background">
+      <div className="relative overflow-hidden h-[90vh] border rounded-xl">
+        <Header />
+        <Particles
+          className="absolute z-10 inset-0"
+          quantity={200}
+          ease={20}
+          color="#ffffff"
+          refresh
+        />
+        <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#6e6e6e2e_1px,transparent_1px),linear-gradient(to_top,#6e6e6e2e_1px,transparent_1px)] bg-[size:48px_48px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
+      </div>
     </div>
   );
 }
