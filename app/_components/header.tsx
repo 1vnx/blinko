6d70a915d40e1f360424c382627dd0d1d815cc44
Daksh Joshi <dakshcodes@gmail.com>
2024-07-24 10:54:39 +0530

    welcome to blinko

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,3 +1,5 @@
 export default function Header() {
-    return <div></div>;
+    return <div style={{ height: "100vh" }} className="flex flex-col justify-center items-center">
+        <h1 className="text-5xl font-bold text-center justify-center">welcome to blinko!</h1>
+    </div>;
 }
