19abb998a210a91c0b5b0cae6f3b69613035e659
Swargaraj Bhowmik <swargarajbhowmik@gmail.com>
2024-07-24 12:26:44 +0530

    chore(landing): add bg

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,20 +1,26 @@
 import Link from "next/link";
 import Image from "next/image";
-import { Button } from "@/components/ui/button";
+
 import { Sparkles } from "lucide-react";
+import { HoverBorderGradient } from "@/components/ui/hover-border-gradient";
 
 export default function Header() {
   return (
     <div className="relative flex justify-between items-center p-4 px-6 border-b z-50">
       <div>
         <Link href="/">
-          <Image src="/blinko.svg" alt="Blibko" width="90" height="50" />
+          <Image src="/blinko.svg" alt="Blinko" width="90" height="50" />
         </Link>
       </div>
       <div>
-        <Button variant="outline">
-          Create Profile <Sparkles className="h-4 w-4 ml-2 text-purple-300" />
-        </Button>
+        <HoverBorderGradient
+          containerClassName="rounded-full"
+          as="button"
+          className="bg-accent dark:text-white flex items-center space-x-2"
+        >
+          <span>Create Profile</span>
+          <Sparkles className="h-4 w-4 ml-2" />
+        </HoverBorderGradient>
       </div>
     </div>
   );
