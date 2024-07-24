63618e7ccf45637f689787dd771f68ff345b3e71
Daksh Joshi <dakshcodes@gmail.com>
2024-07-24 12:03:07 +0530

    grid gredient fix

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Landing() {
           color="#ffffff"
           refresh
         />
-        <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#6e6e6e2e_1px,transparent_1px),linear-gradient(to_top,#6e6e6e2e_1px,transparent_1px)] bg-[size:48px_48px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
+        <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#6e6e6e2e_1px,transparent_1px),linear-gradient(to_top,#6e6e6e2e_1px,transparent_1px)] bg-[size:48px_48px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_100%,#000_70%,transparent_100%)]"></div>
       </div>
     </div>
   );
